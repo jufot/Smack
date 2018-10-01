9c65b4cb86c9c6a62e3939940b2a4f3ac75f8c7c
Jeremiah Ufot <jeremiahufot@jeremiahs-mbp.ncat.edu>
2018-10-01 14:39:49 -0400

    Login page and elements

---
+++ Smack/Controller/LoginVC.swift
@@ -0,0 +1,27 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by Jeremiah Ufot on 10/1/18.
+//  Copyright © 2018 Jeremiah Ufot. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+    
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+}
---
+++ Smack/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Jeremiah Ufot on 10/1/18.
+//  Copyright © 2018 Jeremiah Ufot. All rights reserved.
+//
+
+import Foundation
+
+//Segues
+
+let TO_LOGIN = "toLogin"
